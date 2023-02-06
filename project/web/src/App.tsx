c567c05f665eb587cede4999e7fa873651de739d
IK <adventurer-ik@daum.net>
2023-02-06 20:01:54 +0900

    Navbar 추가

--- project/web/src/App.tsx
+++ project/web/src/App.tsx
@@ -1,23 +1,23 @@
 import * as React from "react"
 import {
   ChakraProvider,
-  Box,
-  Text,
   theme,
 } from "@chakra-ui/react"
 import { ApolloProvider } from "@apollo/client/react"
-import FilmList from "./components/film/FilmList"
 import { createApolloClient } from "./apollo/createApolloClient"
+import { BrowserRouter, Route } from "react-router-dom"
+import Main from "./pages/Main"
 
 const apolloClient = createApolloClient()
 
-export const App: React.FC = () => (
-  <ApolloProvider client={apolloClient}>
-    <ChakraProvider theme={theme}>
-      <Box>
-        <Text><b>Ghibli GraphQL</b></Text>
-        <FilmList></FilmList>
-      </Box>
-    </ChakraProvider>
-  </ApolloProvider>
-)
+export const App: React.FC = () => {
+  return (
+    <ApolloProvider client={apolloClient}>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Route exact path="/" component={Main} />
+        </BrowserRouter>
+      </ChakraProvider>
+    </ApolloProvider>
+  )
+};
---
+++ project/web/src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react';
+import CommonLayout from '../components/CommonLayout';
+import FilmList from '../components/film/FilmList';
+
+export default function Main(): React.ReactElement {
+  return (
+    <CommonLayout>
+      <Heading size="1g">최고의 장면을 찾아보세요.</Heading>
+      <FilmList />
+    </CommonLayout>
+  );
+}
