b5b5c14cb38ecaebf843eeddbdbb47125ae62c3f
IK <adventurer-ik@daum.net>
2023-02-06 18:54:20 +0900

    App.tsx 코드 분리

--- project/web/src/App.tsx
+++ project/web/src/App.tsx
@@ -5,14 +5,11 @@ import {
   Text,
   theme,
 } from "@chakra-ui/react"
-import { ApolloClient, InMemoryCache } from "@apollo/client"
 import { ApolloProvider } from "@apollo/client/react"
 import FilmList from "./components/film/FilmList"
+import { createApolloClient } from "./apollo/createApolloClient"
 
-const apolloClient = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
-})
+const apolloClient = createApolloClient()
 
 export const App: React.FC = () => (
   <ApolloProvider client={apolloClient}>
