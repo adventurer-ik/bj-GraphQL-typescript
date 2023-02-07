286af314e72ef71293e683dffa0f271f01b5ee11
IK <adventurer-ik@daum.net>
2023-02-07 22:15:50 +0900

    이미지 클릭시, 모달 추가

--- project/web/src/components/film/FilmCard.tsx
+++ project/web/src/components/film/FilmCard.tsx
@@ -1,34 +1,40 @@
-import { AspectRatio, Box, Heading, Image, LinkBox, Stack, Text, useColorModeValue } from "@chakra-ui/react";
-import React from 'react'
-import { FilmsQuery } from "../../generated/graphql"; 
+import {
+  AspectRatio,
+  Box,
+  Heading,
+  Image,
+  LinkBox,
+  LinkOverlay,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FilmsQuery } from '../../generated/graphql';
 
 interface FilmCardProps {
   film: FilmsQuery['films']['films'][0];
 }
 
-export default function FilmCard({film}: FilmCardProps): React.ReactElement {
+export default function FilmCard({ film }: FilmCardProps): React.ReactElement {
   return (
     <LinkBox as="article" my={6}>
       <Box maxW={'300px'} w={'full'} rounded={'md'} px={{ base: 1, md: 3 }} pt={3} overflow={'hidden'}>
-        <Box bg={"gray.100"} mt={-3} mx={-3} mb={2} pos="relative">
+        <Box bg={'gray.100'} mt={-3} mx={-3} mb={2} pos="relative">
           <AspectRatio ratio={2 / 3}>
             <Image src={film.posterImg}></Image>
           </AspectRatio>
         </Box>
         <Stack>
-          <Heading
-            color={useColorModeValue('gray.700', 'white')}
-            fontSize="xl"
-            fontFamily={"body"}
-          >
-            {film.title}
-          </Heading>
-          <Text fontSize={"sm"} color={"gray.500"} isTruncated>
-            {film.subtitle ? film.subtitle : <>&nbsp;</>}
-          </Text>
+          <LinkOverlay as={Link} to={`/film/${film.id}`}>
+            <Heading color={useColorModeValue('gray.700', 'white')} fontSize="xl" fontFamily={'body'}>
+              {film.title}
+            </Heading>
+          </LinkOverlay>
         </Stack>
         <Stack spacing={0} fontSize="sm" mt={2}>
-          <Text as={"time"} dateTime={film.release} isTruncated color={"gray.500"}>
+          <Text as={'time'} dateTime={film.release} isTruncated color={'gray.500'}>
             {`${film.release} · ${film.runningTime} 분`}
           </Text>
           <Text isTruncated>{film.director.name}</Text>
--- project/web/src/pages/Film.tsx
+++ project/web/src/pages/Film.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import { Box, Text, Spinner, useDisclosure } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import CommonLayout from '../components/CommonLayout';
+import FilmCutList from '../components/film-cut/FilmCutList';
+import FilmCutModal from '../components/film-cut/FilmCutModal';
+import FilmDetail from '../components/film/FilmDetail';
+import { useFilmQuery } from '../generated/graphql';
 
 interface FilmPageParams {
   filmId: string;
@@ -8,7 +13,36 @@ interface FilmPageParams {
 
 function Film(): React.ReactElement {
   const { filmId } = useParams<FilmPageParams>();
-  return <CommonLayout>영화 개별 상세 정보 페이지 filmId: {filmId}</CommonLayout>;
+  const { data, loading, error } = useFilmQuery({
+    variables: { filmId: Number(filmId) },
+  });
+
+  // Selected cut, Modal states
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedCutId, setSelectedCutId] = useState<number>();
+  const handleCutSelect = (cutId: number) => {
+    setSelectedCutId(cutId); // cut id
+    onOpen(); // modal open
+  };
+
+  return (
+    <CommonLayout>
+      {loading && <Spinner />}
+      {error && <Text>페이지를 표시할 수 없습니다.</Text>}
+      {filmId && data?.film ? (
+        <>
+          <FilmDetail film={data.film} />
+          <Box mt={12}>
+            <FilmCutList filmId={data.film.id} onClick={handleCutSelect} />
+          </Box>
+        </>
+      ) : (
+        <Text>페이지를 표시할 수 없습니다.</Text>
+      )}
+
+      {!selectedCutId ? null : <FilmCutModal open={isOpen} onClose={onClose} cutId={selectedCutId} />}
+    </CommonLayout>
+  );
 }
 
 export default Film;
